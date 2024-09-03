958b398c181cc63ac49335563548612f4af7f2d5
Guilmon-Kyo <martin.gonzalez.nunez@gmail.com>
2024-09-03 17:52:25 -0400

    Arreglo de navbar y main

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -19,12 +19,14 @@
 			</p>
 		</div>
 		<div class="row-span-1">
-			<button>Si</button>
+			<button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-white 
+			font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none 
+			dark:focus:ring-white">Si</button>
 		</div>
 	</div>
 </div>
 
-<div class="categorias col-span-6 bg-amber-600 grid grid-row-1">
+<div class="categorias col-span-6 bg-amber-600 grid grid-rows-1">
 	<div class="row-span-1 grid grid-cols-8">
 		<div class="col-span-2">0</div>
 		<div class="col-span-1">Collares</div>
@@ -35,7 +37,18 @@
 	  </div>
 </div>
 
-<div class="recomendaciones col-span-6 bg-lime-900">
-	RECOMENDACIONES
+<div class="recomendaciones col-span-6 bg-lime-900 grid grid-rows-2">
+	<div class="row-span-1">
+		Recomendaciones
+	</div>
+	<div class="row-span-1 grid grid-cols-6">
+		<div class="col-span-1">01</div>
+		<div class="col-span-1">02</div>
+		<div class="col-span-1">03</div>
+		<div class="col-span-1">04</div>
+		<div class="col-span-1">05</div>
+		<div class="col-span-1">06</div>
+	  </div>
+
 </div>
 @endsection
--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -8,17 +8,17 @@
         @vite('resources/css/app.css')
 
     </head>
-    <body class="grid grid-cols-6 h-screen text-center">
-      <div class="nav col-span-6 bg-teal-500 grid grid-rows-2">
-			    <div class="nav-1 row-span-1 pt-4">
+    <body class="grid grid-cols-6 text-center">
+        <div class="nav col-span-6 bg-teal-500 grid grid-rows-2">
+			<div class="nav-1 row-span-1 pt-4">
                 <div class="grid grid-cols-3 gap-3">
                     <div>01</div>
                     <div>02</div>
                     <div>03</div>
                 </div>
-          </div>
+            </div>
           <hr class="w-screen border-t-2 border-black my-1">
-			    <div class="nav-2 row-span-1 pb-4">
+			<div class="nav-2 row-span-1 pb-4">
                 <div class="grid grid-cols-6 gap-3">
                     <div>04</div>
                     <div>05</div>
@@ -27,13 +27,21 @@
                     <div>08</div>
                     <div>09</div>
                 </div>
-			    </div>
-		  </div>
+			</div>
+		</div>
 
       @yield('contenido')
 
-      <footer class="col-span-6 bg-cyan-600">
-			  FOOTER
-		  </footer>
+        <footer class="col-span-6 bg-cyan-600 grid grid-cols-3">
+			  <div class="col-span-1 text-center">
+                Redes sociales
+              </div>
+              <div class="col-span-1 text-center">
+                Informacion
+              </div>
+              <div class="col-span-1 text-center">
+                Servicio al cliente
+              </div>
+	    </footer>
     </body>
 </html>
