7dad88aa7ae13b64f55606a9d4a6754fcb9db667
Guilmon-Kyo <martin.gonzalez.nunez@gmail.com>
2024-09-02 20:22:52 -0400

    Boceto y posicionamiento de objetos en la navbar y main

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -9,16 +9,27 @@
 
     </head>
     <body class="grid grid-cols-6 h-screen text-center">
-        
-        <div class="nav col-span-6 bg-teal-500">
-			NAVBAR
-			<div class="nav-1">
-
+        <div class="nav col-span-6 bg-teal-500 grid grid-rows-2">
+			<div class="nav-1 row-span-1 pt-4">
+                <div class="grid grid-cols-3 gap-3">
+                    <div>01</div>
+                    <div>02</div>
+                    <div>03</div>
+                </div>
 			</div>
-			<div class="nav-2">
-
+            <hr class="w-screen border-t-2 border-black my-1">
+			<div class="nav-2 row-span-1 pb-4">
+                <div class="grid grid-cols-6 gap-3">
+                    <div>04</div>
+                    <div>05</div>
+                    <div>06</div>
+                    <div>07</div>
+                    <div>08</div>
+                    <div>09</div>
+                </div>
 			</div>
 		</div>
+        
 
         @yield('contenido')
 
