a92e7d96ef046fcc2c4b790c875215af8427971e
Guilmon-Kyo <martin.gonzalez.nunez@gmail.com>
2024-09-05 19:44:45 -0400

    forms en contacto y vista carrito

--- resources/views/contacto/index.blade.php
+++ resources/views/contacto/index.blade.php
@@ -14,7 +14,7 @@
         </div>
     </div>
     
-    <div class="grid grid-cols-3 sm:grid-cols-3 text-center">
+    <div class="grid grid-cols-1 sm:grid-cols-3 text-center">
         <p class="col-span-3">parrafo 1 de datos</p>
         <p class="col-span-3">parrafo 2 de datos</p>
         <p class="col-span-3">parrafo 3 de datos</p>
@@ -25,21 +25,25 @@
         <div class="col-span-1"></div>
         <div class="col-span-1">
             <form class="max-w-sm mx-auto">
-                <div class="my-3">
+                <div class="mb-2">
+                    <input type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+                    dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tu nombre">
+                </div>
+                <div class="mb-2">
                     <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
                     dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tu email" required />
                 </div>
-                <div class="my-3">
+                <div class="mb-2">
                     <input type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
                     dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Telefono (opcional)" />
                 </div>
-                <div class="my-3 flex items-start">
+                <div class="mb-2 flex items-start">
                     <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 
                     dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Dejanos un mensaje"></textarea>
                 </div>
-                <div class="my-3">
+                <div class="mb-2">
                     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
-                 text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Enviar</button>
                 </div>
                 
             </form>
--- resources/views/estado-orden/index.blade.php
+++ resources/views/estado-orden/index.blade.php
@@ -1,9 +1,32 @@
 @extends('template.master')
 
 @section('contenido')
-<div class="col-span-6 grid grid-cols-8 bg-red-600">
-	<div class="col-[3/7] bg-orange-400">
-        Formulario estado de la orden
+
+
+<div class="col-span-6 grid grid-cols-8 grid-rows-4 bg-red-600">
+	<div class="col-start-3 col-end-7 bg-orange-400 text-3xl"></div>
+    <div class="col-start-3 col-end-7 bg-orange-400">
+        <div class="text-3xl">Formulario estado de la orden</div>
+        <hr class="mx-auto max-w-sm border-black">
+        <br>
+        <div class="mx-auto max-w-sm">Puedes colocar tus datos para ver el estado de tu pedido! </div>
     </div>
+    <div class="col-start-3 col-end-7 bg-orange-400 ">
+        <div class="mb-2 max-w-sm mx-auto">
+            <input type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+            dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Telefono (opcional)" />
+        </div>
+        <div class="mb-2 max-w-sm mx-auto">
+            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+            dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tu email" required />
+        </div>
+        <div class="mb-2">
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full 
+         text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ver estado del pedido</button>
+        </div>
+    </div>
+    <div class="col-start-3 col-end-7 bg-orange-400"></div>
 </div>
+
+
 @endsection
